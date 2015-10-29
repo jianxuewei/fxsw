9070036242beb3d89613b36bbdbc86fcda432632
jianxuewei <jianxuewei@126.com>
2015-10-29 15:49:19 +0800

    test broadcast and receiver ok

--- app/src/main/java/com/fxsw/fxsw/BootCompleteReceiver.java
+++ app/src/main/java/com/fxsw/fxsw/BootCompleteReceiver.java
@@ -15,7 +15,7 @@ public class BootCompleteReceiver extends BroadcastReceiver {
         if(intent.getAction().equals(ACTION)){
             Log.d("debug","Action is "+ACTION);
             Intent intentStartGuideAcitivity=new Intent(context,GuideActivity.class);
-            intentStartGuideAcitivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intentStartGuideAcitivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED|Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
 
             context.startActivity(intentStartGuideAcitivity);
         }
